160d7b921cfddb22f6fe4fe3b779d07afd91c155
srinivaspradhan64 <srinivaspradhan@gmail.com>
2026-05-04 21:04:43 -0400

    Add startup and shutdown event

--- payment-service/main.py
+++ payment-service/main.py
@@ -11,3 +11,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+@app.on_event("startup")
+async def startup_event():
+    print("Startup now.")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("Shutdown now.")
